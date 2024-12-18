00fbc2c0b932dbf4bfaa9de87319d9e7f7b5a552
SubornaTamanna <subornatamanna21@gmail.com>
2024-12-18 14:57:05 +0600

    first commit

---
+++ resturant.js
@@ -0,0 +1,7 @@
+let main_button=Document.geteElementbyid(main_button)
+let main_menu=Document.geteElementbyid(main_button)
+
+
+main_button.addEventListener(click,0=>{
+    main_menu.classList.toggle
+})
